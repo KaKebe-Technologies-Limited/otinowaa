5ecb168fcd4c0814b2152be77c69c715e05e990f
OlUK Mark <holmark5@gmail.com>
2022-02-21 12:33:03 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -33,4 +33,46 @@
             <!-- <h5 class="text-white">OTW BROWSER</h5> -->
         </div<>
     </header>
+     <!-- Mobile Navigation -->
+ <nav id="mobile-nav" class="navbar navbar-light navbar-expand-sm">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php"><img src="images/logobg.png" width="70%" height="30px" alt=""></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                        <ul class="navbar-nav">
+                            <li class="nav-item center dropdown">
+                                <a class="nav-link text-dark text-bold" href="index.php">Dashboard</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle text-dark text-bold d-flex align-items-center" href="cis.php" role="button" data-bs-toggle="dropdown">CIS</a>
+                                <ul class="dropdown-menu">
+                                    <li class="list-unstyled"><a class="dropdown-item" href="cis.php">Add Child</a>
+                                    </li class="list-unstyled"><a class="dropdown-item" href="view_all.php">View All</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle text-dark text-bold d-flex align-items-center" href="#" role="button" data-bs-toggle="dropdown">User Manager</a>
+                                <ul class="dropdown-menu">
+                                    <li class="list-unstyled"><a class="dropdown-item" href="#">Add Staff</a></li>
+                                    <li class="list-unstyled"><a class="dropdown-item" href="#">View All</a></li>
+                                    <li class="list-unstyled"><a class="dropdown-item" href="#">Roles</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <div class="center">
+                                    <a class="nav-link text-dark text-bold" href="#" role="button" data-bs-toggle="dropdown">My Profile</a>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <div class="center">
+                                    <a href="#" class="nav-link text-bold text-dark">EXIT</a>
+                                </div>
+                            </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!--/Mobile Navigation-->
 <main class="container-fluid p-0 d-flex">
